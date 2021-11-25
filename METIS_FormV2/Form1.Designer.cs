b3a96c4507ab1e56e6ae333176d98fcfce7d6a39
tadamo3 <88508819+tadamo3@users.noreply.github.com>
2021-11-25 18:49:55 -0500

    Console C#
    Première version de la console C# de récupération de données.
    
    Fonctions implémentées : 
       - Lecture de valeurs envoyées par communication Serial via l'Arduino
       - Affichage de ces valeurs sur un graphique real time

---
+++ METIS_FormV2/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace METIS_FormV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.cbComPorts = new System.Windows.Forms.ComboBox();
+            this.cbBaudRate = new System.Windows.Forms.ComboBox();
+            this.lbComPorts = new System.Windows.Forms.Label();
+            this.lbBaudRate = new System.Windows.Forms.Label();
+            this.btnOpenPort = new System.Windows.Forms.Button();
+            this.btnClosePort = new System.Windows.Forms.Button();
+            this.txbADC = new System.Windows.Forms.TextBox();
+            this.lbADC = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.chartData = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbComPorts
+            // 
+            this.cbComPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbComPorts.FormattingEnabled = true;
+            this.cbComPorts.Location = new System.Drawing.Point(141, 43);
+            this.cbComPorts.Name = "cbComPorts";
+            this.cbComPorts.Size = new System.Drawing.Size(206, 33);
+            this.cbComPorts.TabIndex = 0;
+            this.cbComPorts.DropDown += new System.EventHandler(this.cbComPorts_DropDown);
+            // 
+            // cbBaudRate
+            // 
+            this.cbBaudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbBaudRate.FormattingEnabled = true;
+            this.cbBaudRate.Items.AddRange(new object[] {
+            "9600",
+            "57600",
+            "115200"});
+            this.cbBaudRate.Location = new System.Drawing.Point(525, 43);
+            this.cbBaudRate.Name = "cbBaudRate";
+            this.cbBaudRate.Size = new System.Drawing.Size(206, 33);
+            this.cbBaudRate.TabIndex = 1;
+            // 
+            // lbComPorts
+            // 
+            this.lbComPorts.AutoSize = true;
+            this.lbComPorts.Location = new System.Drawing.Point(6, 46);
+            this.lbComPorts.Name = "lbComPorts";
+            this.lbComPorts.Size = new System.Drawing.Size(129, 25);
+            this.lbComPorts.TabIndex = 2;
+            this.lbComPorts.Text = "COM Ports :";
+            // 
+            // lbBaudRate
+            // 
+            this.lbBaudRate.AutoSize = true;
+            this.lbBaudRate.Location = new System.Drawing.Point(402, 46);
+            this.lbBaudRate.Name = "lbBaudRate";
+            this.lbBaudRate.Size = new System.Drawing.Size(117, 25);
+            this.lbBaudRate.TabIndex = 3;
+            this.lbBaudRate.Text = "Baud rate :";
+            // 
+            // btnOpenPort
+            // 
+            this.btnOpenPort.Location = new System.Drawing.Point(17, 49);
+            this.btnOpenPort.Name = "btnOpenPort";
+            this.btnOpenPort.Size = new System.Drawing.Size(159, 65);
+            this.btnOpenPort.TabIndex = 4;
+            this.btnOpenPort.Text = "Open Port";
+            this.btnOpenPort.UseVisualStyleBackColor = true;
+            this.btnOpenPort.Click += new System.EventHandler(this.btnOpenPort_Click);
+            // 
+            // btnClosePort
+            // 
+            this.btnClosePort.Location = new System.Drawing.Point(17, 132);
+            this.btnClosePort.Name = "btnClosePort";
+            this.btnClosePort.Size = new System.Drawing.Size(159, 65);
+            this.btnClosePort.TabIndex = 5;
+            this.btnClosePort.Text = "Close Port";
+            this.btnClosePort.UseVisualStyleBackColor = true;
+            this.btnClosePort.Click += new System.EventHandler(this.btnClosePort_Click);
+            // 
+            // txbADC
+            // 
+            this.txbADC.Location = new System.Drawing.Point(361, 57);
+            this.txbADC.Name = "txbADC";
+            this.txbADC.Size = new System.Drawing.Size(201, 31);
+            this.txbADC.TabIndex = 6;
+            this.txbADC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbADC
+            // 
+            this.lbADC.AutoSize = true;
+            this.lbADC.Location = new System.Drawing.Point(226, 60);
+            this.lbADC.Name = "lbADC";
+            this.lbADC.Size = new System.Drawing.Size(129, 25);
+            this.lbADC.TabIndex = 7;
+            this.lbADC.Text = "ADC Value :";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbComPorts);
+            this.groupBox1.Controls.Add(this.cbComPorts);
+            this.groupBox1.Controls.Add(this.cbBaudRate);
+            this.groupBox1.Controls.Add(this.lbBaudRate);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(0, 822);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1474, 107);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.chartData);
+            this.groupBox4.Controls.Add(this.btnOpenPort);
+            this.groupBox4.Controls.Add(this.btnClosePort);
+            this.groupBox4.Controls.Add(this.lbADC);
+            this.groupBox4.Controls.Add(this.txbADC);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox4.Location = new System.Drawing.Point(0, 0);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(1474, 816);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Acquire Data";
+            // 
+            // chartData
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartData.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartData.Legends.Add(legend1);
+            this.chartData.Location = new System.Drawing.Point(27, 210);
+            this.chartData.Name = "chartData";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Data";
+            this.chartData.Series.Add(series1);
+            this.chartData.Size = new System.Drawing.Size(1406, 600);
+            this.chartData.TabIndex = 8;
+            this.chartData.Text = "Data";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1474, 929);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbComPorts;
+        private System.Windows.Forms.ComboBox cbBaudRate;
+        private System.Windows.Forms.Label lbComPorts;
+        private System.Windows.Forms.Label lbBaudRate;
+        private System.Windows.Forms.Button btnOpenPort;
+        private System.Windows.Forms.Button btnClosePort;
+        private System.Windows.Forms.TextBox txbADC;
+        private System.Windows.Forms.Label lbADC;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartData;
+    }
+}
+
---
+++ METIS_FormV2/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace METIS_FormV2
+{
+    public partial class Form1 : Form
+    {
+        #region Variables
+        int serialDataIn;
+        int index = 0;
+        int[] dataArray = new int[50];
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnOpenPort.Enabled = true;
+            btnClosePort.Enabled = false;
+            cbBaudRate.Text = "9600";
+            txbADC.Enabled = false;
+
+            chartData.Series["Data"].Points.AddXY(1, 1);
+        }
+
+        #region Buttons
+        private void btnOpenPort_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = cbComPorts.Text;
+                serialPort1.BaudRate = Convert.ToInt32(cbBaudRate.Text);
+                serialPort1.Open();
+
+                btnOpenPort.Enabled = false;
+                btnClosePort.Enabled = true;
+                cbComPorts.Enabled = false;
+                cbBaudRate.Enabled = false;
+
+                chartData.Series["Data"].Points.Clear();
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void btnClosePort_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+
+                btnOpenPort.Enabled = true;
+                btnClosePort.Enabled = false;
+                cbComPorts.Enabled = true;
+                cbBaudRate.Enabled = true;
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+        #endregion
+
+        #region ComboBox
+        private void cbComPorts_DropDown(object sender, EventArgs e)
+        {
+            string[] portsList = SerialPort.GetPortNames();
+            cbComPorts.Items.Clear();
+            cbComPorts.Items.AddRange(portsList);
+        }
+
+
+
+
+        #endregion
+
+        #region Data Reception
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            serialDataIn = serialPort1.ReadByte();
+            this.BeginInvoke(new EventHandler(UpdateArray));
+        }
+
+        private void UpdateArray(object sender, EventArgs e)
+        {
+            try
+            {
+                if (index < dataArray.Length - 1)
+                {
+                    chartData.Series["Data"].Points.AddY(serialDataIn);
+                    index++;
+                }
+                else
+                {
+                    ShiftData(dataArray, serialDataIn);
+                    chartData.Series["Data"].Points.Clear();
+                    for (int i = 0; i < dataArray.Length - 1; i++)
+                    {
+                        chartData.Series["Data"].Points.AddY(dataArray[i]);
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void ShiftData(int[] array, int newValue)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                array[i - 1] = array[i];
+                array[array.Length - 1] = newValue;
+            }
+        }
+
+        #endregion
+    }
+}
