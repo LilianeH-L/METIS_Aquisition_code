eb32d2a09c85fde41cab67368ccf1b1d2a74944e
tadamo3 <88508819+tadamo3@users.noreply.github.com>
2021-11-30 15:05:08 -0500

    Add files via upload

--- ReceiveDataTeensy/src/main.cpp
+++ ReceiveDataTeensy/src/main.cpp
@@ -1,17 +1,57 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+const int ledPin = 5;
 const int analogPin = 15;
 uint16_t value = 0;
+byte command = 0;
+bool counter = false;
+
+void checkForCommand()
+{
+  if(Serial.available() > 0)
+  {
+    command = Serial.read();
+  }
+}
 
 
 void setup() {
   Serial.begin(115200);
   analogReadResolution(12);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-    value = analogRead(analogPin);
-    Serial.print((String)value + "%\n");
-    delay(50);
+  while(!counter)
+  {
+    checkForCommand();
+    if(command != 0)
+    {
+      counter = true;
+    }
+  }
+
+  switch(command)
+  {
+    case 1:
+      value = analogRead(analogPin);
+      Serial.print((String)value + "%\n");
+      delay(10);
+
+      checkForCommand();
+    break;
+
+    case 2:
+      digitalWrite(ledPin, HIGH);
+      delay(1);
+
+      checkForCommand();
+      if(command != 2)
+      {
+        digitalWrite(ledPin, LOW);
+      }
+    break;
+  }
 }
+
