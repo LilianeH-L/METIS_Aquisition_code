51d4d6b58357e4f777bd47aa42c0fa5c3565c635
tadamo3 <88508819+tadamo3@users.noreply.github.com>
2022-02-24 21:40:21 -0500

    Update main.py to add encod1
    Plot simultané de la valeur de l'électrode_1 et l'encodeur_1. Ajout de commentaires pour clarifier le code.

--- PythonFile/main.py
+++ PythonFile/main.py
@@ -3,46 +3,54 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as plticker
 import numpy as np
 
-temps=[]
-donnee=[]
+#Initialize datafram values
+from numpy import long
+
+temps = []
+electrode_1 = []
+electrode_2 = []
+encod_1 = []
 
 
+with open("Acquisition-1.txt") as f:
+    compteur = 0 #Get count to increment time values
 
+    lines = f.readlines() #Read lines from .txt file
 
-with open("Acquisition-2.txt") as f:
-    compteur=0
-    lines=f.readlines()
+    #Iterate through lines of .txt file
+    #Replace \n and seperators
+    #.txt file is contructed as such :
+        # time | electrod_1 | electrod_2 | ... | electrod_n | encod_1 | encod_2 | ... | encod_n
     for element in lines:
-        element=element.replace('\n','')
-        x=element.split(" | ")
+        element = element.replace('\n', '')
+        x = element.split(" | ")
         temps.append(int(x[0]))
-        donnee.append(int(x[1]))
+        electrode_1.append(int(x[1]))
+        encod_1.append(long(x[3]))
 
+    #Iterate through time column to get time variation
+    #Delete last value in new list - last value is the only one not modified by loop
     for i in range(len(temps)-1):
-        variation= temps[i+1]-temps[i]
-        compteur+=variation
-        temps[i]=compteur
+        variation = temps[i+1]-temps[i]
+        compteur += variation
+        temps[i] = compteur/1000
+
     del temps[-1]
-    del donnee[-1]
+    del electrode_1[-1]
+    del encod_1[-1]
 
-data={'temps': temps , 'donnée': donnee}
-df= pd.DataFrame(data)
-print(df)
+data = {'temps': temps, 'electrode_1': electrode_1, 'encod_1': encod_1}
+df = pd.DataFrame(data)
 
-ax = plt.gca()
-ax.set_ylim([0, 4094])
-df.plot(kind='line', x= 'temps', y='donnée', ax=ax)
+df.plot.line(x='temps')
+#plt.ylim(0, 4096)
+plt.xlabel('Temps (s)')
 plt.ylabel('Données')
 plt.show()
 
-data1= np.asarray([donnee])
+data1= np.asarray([electrode_1])
 np.save('data.npy', data1)
 
 data2=np.load('data.npy')
 plt.plot(data2[0])
 plt.show()
-
-
-
-
-
